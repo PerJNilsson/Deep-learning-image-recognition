28f14696571bac607984e34580398562ed4e82ff
95ep <erik.ao.persson@gmail.com>
2018-02-09 22:54:24 +0100

    More or less working version of a ConvNet for GTSRB

--- Erik/ConvNet_1_1.py
+++ Erik/ConvNet_1_1.py
@@ -1,17 +1,21 @@
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Conv2D
-from keras.layers.pooling import MaxPooling2D
-from keras.optimizers import SGD
-from keras import backend as K
-from skimage import transform
+import glob
+import os
+
 import numpy as np
+from keras.layers.convolutional import Conv2D
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers.pooling import MaxPooling2D
+from keras.models import Sequential
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from keras import backend as K
+from skimage import transform, io
 
 IMG_SIZE = 48
 NUM_CLASSES = 43
 
+
 # Preprocessing with only crop and standard size
-def pre_process(img):
+def preprocess_img(img):
     # Central square crop
     min_side = min(img.shape[:-1])
     centre = img.shape[0] // 2, img.shape[1] // 2
@@ -27,6 +31,13 @@ def pre_process(img):
 
     return img
 
+
+# Returns the class by splitting the path - folder identifies class.
+def get_class(img_path):
+    return int(img_path.split('/')[-2])
+
+
+# Function that defines the structure of the network
 def conv_net():
     model = Sequential()
     model.add(Conv2D(32, (3, 3), padding='same',
@@ -53,3 +64,48 @@ def conv_net():
     model.add(Dropout(0.5))
     model.add(Dense(NUM_CLASSES, activation='softmax'))
     return model
+
+
+def lr_schedule(epoch):
+    return lr * (0.1 ** int(epoch / 10))
+
+
+# Imports and shuffles the images. Will only work with the path to the GTSRB Training directory.
+def import_training_imgs(path):
+    imgs = []
+    labels = []
+
+    all_img_paths = glob.glob(os.path.join(path, '*/*.ppm'))
+    np.random.shuffle(all_img_paths)
+
+    for img_path in all_img_paths:
+        img = preprocess_img(io.imread(img_path))
+        label = get_class(img_path)
+        imgs.append(img)
+        labels.append(label)
+
+    x = np.array(imgs, dtype='float32')
+    y = np.eye(NUM_CLASSES, dtype='uint8')[labels]
+    return x, y
+
+
+# Main program
+training_path = 'path_to_training_set'
+# Parameters for training
+lr = 0.01
+batch_size = 32
+epochs = 30
+
+K.set_image_data_format('channel_first')
+model = conv_net()
+
+X, Y = import_training_imgs(training_path)
+# training
+model.fit(X, Y,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_split=0.2,
+          callbacks=[LearningRateScheduler(lr_schedule),
+                     ModelCheckpoint('Trained_models/model1_1.h5', save_best_only=True)])
+
+
