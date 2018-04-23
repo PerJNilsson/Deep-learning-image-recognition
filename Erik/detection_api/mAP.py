ce82091b93d607d6aa3baf66a75e85c0e843edc5
95ep <erik.ao.persson@gmail.com>
2018-04-23 09:01:53 +0200

    mAP almost done. At least one bug to fix first

---
+++ Erik/detection_api/mAP.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+
+def intersection_over_union(pred, gt):
+    # Coordinates of intersecting rectangle
+    print(pred)
+    print(gt)
+    xA = max(pred[0], gt[0])
+    yA = max(pred[1], gt[1])
+    xB = min(pred[2], gt[2])
+    yB = min(pred[3], gt[3])
+
+    interArea = (xB - xA) * (yB - yA)
+
+    predArea = (pred[2] - pred[0]) * (pred[3] - pred[1])
+    gtArea = (gt[2] - gt[0]) * (gt[3] - gt[1])
+
+    iou = interArea / (predArea + gtArea - interArea)
+    return iou
+
+
+def removeFromMap(filename, index_to_remove):
+    old_sign_list = gt_map[filename]
+    if len(old_sign_list) == 1:
+        del gt_map[filename]
+    else:
+        new_sign_list = []
+        for i in range(0, len(old_sign_list)):
+            if i != index_to_remove:
+                new_sign_list.append(old_sign_list)
+        gt_map[file] = new_sign_list
+
+
+GT_PATH = '/Users/erikpersson/PycharmProjects/Deep-learning-image-recognition/Erik/detection_api/' \
+          'data/TestGTSDB/gt.txt'
+RESULT_PATH = '/Users/erikpersson/PycharmProjects/Deep-learning-image-recognition/Erik/detection_api/' \
+              'data/results/cloud/combined_test/result.csv'
+
+gt_map = dict()
+
+gt = pd.read_csv(GT_PATH, sep = ';', header = None)
+result = pd.read_csv(RESULT_PATH, sep = ';', header = None)
+false_positive = np.zeros(43, dtype=int)
+positive = np.zeros(43, dtype=int)
+
+for i in range(0, len(gt)):
+    file = gt[0][i]
+    bbox = [gt[1][i], gt[2][i], gt[3][i], gt[4][i]]
+    sign_class = gt[5][i]
+    if file not in gt_map:
+        gt_map[file] = [[bbox, sign_class]]
+    else:
+        gt_map[file].append([bbox, sign_class])
+
+for i in range(0, len(result)):
+    file = result[0][i]
+    bbox = [result[1][i], result[2][i], result[3][i], result[4][i]]
+    sign_class = result[5][i]
+    if file in gt_map:
+        gt_signs = gt_map[file]
+        for j in range(0, len(gt_signs)):
+            iou = intersection_over_union(bbox, gt_signs[j][0])
+            if iou > 0.5 and sign_class == gt_signs[j][1]:
+                positive[sign_class] = positive[sign_class] + 1
+                removeFromMap(file, j)
+                break
+            else:
+                false_positive[sign_class] = false_positive[sign_class] + 1
+    else:
+        false_positive[sign_class] = false_positive[sign_class] + 1
+
